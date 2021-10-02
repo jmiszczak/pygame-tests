6e48ed5ab7093a15ece1252234e9e7038f3f312e
Jarek Miszczak <jarek@miszczak.eu>
2021-10-02 17:24:23 +0200

    Simple Shooting game in LOVE

---
+++ love1/main.lua
@@ -0,0 +1,58 @@
+debug = true
+
+player = { x = 200, y = 710, speed = 150, img = nil}
+canShoot = true
+canShootTimerMax = 0.2
+canShootTimer = canShootTimerMax
+
+bulletImg = nil
+bullets = {}
+
+function love.load(arg)
+  player.img = love.graphics.newImage('assets/plane.png')
+  bulletImg = love.graphics.newImage('assets/bullet.png')
+end
+
+function love.update(dt)
+  canShootTimer = canShootTimer - (1 * dt)
+  if canShootTimer < 0 then
+    canShoot = true
+  end
+
+  if love.keyboard.isDown('escape') then
+    love.event.push('quit')
+  end
+
+  if love.keyboard.isDown('left', 'a') then
+    if player.x > 0 then
+      player.x = player.x - (player.speed*dt)
+    end
+  elseif love.keyboard.isDown('right', 'd') then
+    if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+      player.x = player.x + (player.speed*dt)
+    end
+  end
+
+  if love.keyboard.isDown('space', 'rctrl', 'lctrl') and canShoot then
+    newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
+    table.insert(bullets, newBullet)
+    canShoot = false
+    canShootTimer = canShootTimerMax
+  end
+
+ for i, bullet in ipairs(bullets) do
+   bullet.y = bullet.y - (250*dt)
+   if bullet.y < 0 then
+     table.remove(bullets, i)
+   end
+ end
+
+end
+
+function love.draw(dt)
+  -- love.graphics.print("Hello World!", 400, 300)
+  love.graphics.draw(player.img,player.x,player.y)
+  for i, bullet in ipairs(bullets) do
+    love.graphics.draw(bullet.img, bullet.x, bullet.y)
+  end
+end
