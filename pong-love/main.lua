30b2d93283d3b3579c01ef221eb9aa5640228131
Jarek Miszczak <jarek@miszczak.eu>
2021-10-17 17:23:38 +0200

    Moving abd bounbcing ball

--- pong-love/main.lua
+++ pong-love/main.lua
@@ -1,13 +1,14 @@
 -- two players and one ball
 playerLeft = { x = 50, y = 100, speed = 150, img = nil }
 playerRight = { x = 720, y = 200, speed = 150, img = nil }
-ball = { x = 400, y = 300, speed = 150, img = nil }
+ball = { x = 400, y = 300, speedX = 150, speedY = 150, img = nil }
 
 -- load assets
 function love.load(arg)
   playerLeft.img = love.graphics.newImage('assets/paddle-blue.png') 
   playerRight.img = love.graphics.newImage('assets/paddle-green.png') 
   ball.img = love.graphics.newImage('assets/ball.png')
+  courtImg = love.graphics.newImage('assets/court.png')
 end
 
 -- main functionality
@@ -43,12 +44,35 @@ function love.update(dt)
     end
   end
 
+  -- ball movements
+  if ball.x < 0 or ball.x > love.graphics.getWidth() - ball.img:getWidth()  then
+    ball.speedX = -1*ball.speedX
+  end
+
+  if ball.y < 0 or ball.y > love.graphics.getHeight() - ball.img:getHeight()  then
+    ball.speedY = -1*ball.speedY
+  end
+
+
+  ball.x = ball.x + ball.speedX*dt
+  ball.y = ball.y + ball.speedY*dt
+
 end
 
 -- redraw the screen
 function love.draw(dt)
+
+  love.graphics.draw(courtImg, 0,0 )
   love.graphics.draw(ball.img, ball.x, ball.y)
   love.graphics.draw(playerLeft.img, playerLeft.x, playerLeft.y)
   love.graphics.draw(playerRight.img, playerRight.x, playerRight.y)
 
+end
+
+-- basic checking for collisions
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
 end
